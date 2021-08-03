9abcaa1baeef07dc5745e2b1b0894c46729c34a5
brimorales <bmorales@loopstudio.dev>
2021-08-03 16:30:56 -0300

    Pagination of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
@@ -10,7 +11,6 @@ Rails.application.routes.draw do
   end
   resources :categories
   resources :users, only: [:update]
-
 
 
 
